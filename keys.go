21eb286f6e1eeffcf3b786840e5707fdb59e9cd6
xuyu <xuyu16@gmail.com>
2013-09-02 21:13:35 +0800

    add the rest of keys command[ignore apart]

--- keys.go
+++ keys.go
@@ -65,3 +65,91 @@ func (r *Redis) Keys(pattern string) ([]string, error) {
 	}
 	return result, nil
 }
+
+func (r *Redis) Move(key string, db int) (bool, error) {
+	if err := r.send_command("MOVE", key, strconv.Itoa(db)); err != nil {
+		return false, err
+	}
+	return r.bool_reply()
+}
+
+func (r *Redis) Persist(key string) (bool, error) {
+	if err := r.send_command("PERSIST", key); err != nil {
+		return false, err
+	}
+	return r.bool_reply()
+}
+
+func (r *Redis) Pexpire(key string, milliseconds int) (bool, error) {
+	if err := r.send_command("PEXPIRE", key, strconv.Itoa(milliseconds)); err != nil {
+		return false, err
+	}
+	return r.bool_reply()
+}
+
+func (r *Redis) Pexpireat(key string, timestamp int) (bool, error) {
+	if err := r.send_command("PEXPIREAT", key, strconv.Itoa(timestamp)); err != nil {
+		return false, err
+	}
+	return r.bool_reply()
+}
+
+func (r *Redis) Pttl(key string) (int, error) {
+	if err := r.send_command("PTTL", key); err != nil {
+		return -1, err
+	}
+	return r.integer_reply()
+}
+
+func (r *Redis) RandomKey() (string, error) {
+	if err := r.send_command("RANDOMKEY"); err != nil {
+		return "", err
+	}
+	bulk, err := r.bulk_reply()
+	if err != nil {
+		return "", err
+	}
+	if bulk == nil {
+		return "", nil
+	}
+	return *bulk, nil
+}
+
+func (r *Redis) Rename(key, newkey string) error {
+	if err := r.send_command("RENAME", key, newkey); err != nil {
+		return err
+	}
+	_, err := r.status_reply()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Redis) Renamenx(key, newkey string) (bool, error) {
+	if err := r.send_command("RENAMENX", key, newkey); err != nil {
+		return false, err
+	}
+	return r.bool_reply()
+}
+
+func (r *Redis) Restore(key string, ttl int, serialized string) error {
+	if err := r.send_command("RESTORE", key, strconv.Itoa(ttl), serialized); err != nil {
+		return err
+	}
+	return r.ok_reply()
+}
+
+func (r *Redis) TTL(key string) (int, error) {
+	if err := r.send_command("TTL", key); err != nil {
+		return -1, err
+	}
+	return r.integer_reply()
+}
+
+func (r *Redis) Type(key string) (string, error) {
+	if err := r.send_command("TYPE", key); err != nil {
+		return "", err
+	}
+	return r.status_reply()
+}
