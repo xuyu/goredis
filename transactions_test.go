7b1c3cebde9225766e7313fcdc15a09b1f8dd230
xuyu <xuyu16@gmail.com>
2014-01-04 15:36:17 +0800

    add more transaction test case

--- transactions_test.go
+++ transactions_test.go
@@ -10,10 +10,18 @@ func TestTransaction(t *testing.T) {
 		t.Error(err)
 	}
 	defer transaction.Close()
-	transaction.Command("DEL", "key")
-	transaction.Command("SET", "key", 1)
-	transaction.Command("INCR", "key")
-	transaction.Command("GET", "key")
+	if err := transaction.Command("DEL", "key"); err != nil {
+		t.Error(err)
+	}
+	if err := transaction.Command("SET", "key", 1); err != nil {
+		t.Error(err)
+	}
+	if err := transaction.Command("INCR", "key"); err != nil {
+		t.Error(err)
+	}
+	if err := transaction.Command("GET", "key"); err != nil {
+		t.Error(err)
+	}
 	result, err := transaction.Exec()
 	if err != nil {
 		t.Error(err)
@@ -23,5 +31,40 @@ func TestTransaction(t *testing.T) {
 	}
 	if s, err := result[3].StringValue(); err != nil || s != "2" {
 		t.Fail()
+	}
+}
+
+func TestWatch(t *testing.T) {
+	transaction, err := r.Transaction()
+	if err != nil {
+		t.Error(err)
+	}
+	defer transaction.Close()
+	if err := transaction.Watch("key"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestUnWatch(t *testing.T) {
+	transaction, err := r.Transaction()
+	if err != nil {
+		t.Error(err)
+	}
+	defer transaction.Close()
+	transaction.Watch("key")
+	if err := transaction.UnWatch(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDiscard(t *testing.T) {
+	transaction, err := r.Transaction()
+	if err != nil {
+		t.Error(err)
+	}
+	defer transaction.Close()
+	transaction.Command("SET", "KEY", 1)
+	if transaction.Discard(); err != nil {
+		t.Error(err)
 	}
 }
