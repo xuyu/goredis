3fdee788074a13eae1e7b898e3e036bc1b89d373
xuyu <xuyu16@gmail.com>
2014-01-07 17:21:05 +0800

    add more sorted sets test case

--- sorted_sets_test.go
+++ sorted_sets_test.go
@@ -94,3 +94,125 @@ func TestZRange(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestZRank(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if n, err := r.ZRank("key", "three"); err != nil {
+		t.Error(err)
+	} else if n != 2 {
+		t.Fail()
+	}
+	if n, err := r.ZRank("key", "four"); err != nil {
+		t.Error(err)
+	} else if n >= 0 {
+		t.Fail()
+	}
+}
+
+func TestZRem(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if n, err := r.ZRem("key", "three", "four"); err != nil {
+		t.Error(err)
+	} else if n != 1 {
+		t.Fail()
+	}
+}
+
+func TestZRemRangeByRank(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if n, err := r.ZRemRangeByRank("key", 0, 1); err != nil {
+		t.Error(err)
+	} else if n != 2 {
+		t.Fail()
+	}
+}
+
+func TestZRemRangeByScore(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if n, err := r.ZRemRangeByScore("key", "-inf", "(2"); err != nil {
+		t.Error(err)
+	} else if n != 1 {
+		t.Fail()
+	}
+}
+
+func TestZRevRange(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if result, err := r.ZRevRange("key", 0, -1, false); err != nil {
+		t.Error(err)
+	} else if len(result) != 3 {
+		t.Fail()
+	} else if result[0] != "three" {
+		t.Fail()
+	}
+}
+
+func TestZRevRank(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if n, err := r.ZRevRank("key", "three"); err != nil {
+		t.Error(err)
+	} else if n != 0 {
+		t.Fail()
+	}
+	if n, err := r.ZRevRank("key", "four"); err != nil {
+		t.Error(err)
+	} else if n >= 0 {
+		t.Fail()
+	}
+}
+
+func TestZScore(t *testing.T) {
+	r.Del("key")
+	pairs := map[string]float64{
+		"one":   1.0,
+		"two":   2.0,
+		"three": 3.0,
+	}
+	r.ZAdd("key", pairs)
+	if result, err := r.ZScore("key", "member"); err != nil {
+		t.Error(err)
+	} else if result != nil {
+		t.Fail()
+	}
+	if result, err := r.ZScore("key", "two"); err != nil {
+		t.Error(err)
+	} else if string(result) != "2" {
+		t.Fail()
+	}
+}
