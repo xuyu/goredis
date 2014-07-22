07aaaf7ae1d1fb7369dd6919f3181cc8707e7367
Bill Anderson <therealbill@me.com>
2014-07-22 18:08:50 -0500

    added remaining fields to MasterInfo struct
    Also improved some of the documentation of the struct

--- sentinel.go
+++ sentinel.go
@@ -15,11 +15,53 @@ type MasterAddress struct {
 
 // MasterInfo is a struct providing the information available from sentinel
 // about a given master (aka pod)
+// The way this works is you tag the field with the name redis returns
+// and reflect is used in the methods which return this structure to populate
+// it with the data from Redis
+//
+// Note this means it will nee dto be updated when new fields are added in
+// sentinel. Fortunately this appears to be rare.
+//
+// Currently the list is:
+// 'pending-commands'
+// 'ip'
+// 'down-after-milliseconds'
+// 'role-reported'
+// 'runid'
+// 'port'
+// 'last-ok-ping-reply'
+// 'last-ping-sent'
+// 'failover-timeout'
+// 'config-epoch'
+// 'quorum'
+// 'role-reported-time'
+// 'last-ping-reply'
+// 'name'
+// 'parallel-syncs'
+// 'info-refresh'
+// 'flags'
+// 'num-slaves'
+// 'num-other-sentinels'
 type MasterInfo struct {
-	Name      string `redis:"name"`
-	Port      int    `redis:"port"`
-	NumSlaves int    `redis:"num-slaves"`
-	Quorum    int    `redis:"quorum"`
+	Name                  string `redis:"name"`
+	Port                  int    `redis:"port"`
+	NumSlaves             int    `redis:"num-slaves"`
+	Quorum                int    `redis:"quorum"`
+	NumOtherSentinels     int    `redis:"num-other-sentinels"`
+	ParallelSyncs         int    `redis:"parallel-syncs"`
+	RoleReported          string `redis:"role-reported"`
+	Runid                 string `redis:"runid"`
+	IP                    string `redis:"ip"`
+	DownAfterMilliseconds int    `redis:"down-after-milliseconds"`
+	IsMasterDown          bool   `redis:"is-master-down"`
+	LastOkPingReply       int    `redis:"last-ok-ping-reply"`
+	LastPingSent          int    `redis:"last-ping-sent"`
+	FailoverTimeout       int    `redis:"failover-timeout"`
+	ConfigEpoch           int    `redis:"config-epoch"`
+	RoleReportedTime      int    `redis:"role-reported-time"`
+	LastPingReply         int    `redis:"last-ping-reply"`
+	InfoRefresh           int    `redis:"info-refresh"`
+	Flags                 string `redis:"flags"`
 }
 
 // SentinelMonitor executes the SENTINEL MONITOR command on the server
@@ -59,6 +101,21 @@ func (r *Redis) SentinelMasterInfo(podname string) (master MasterInfo, err error
 		}
 		if f.Type().Name() == "string" {
 			f.SetString(info[tag])
+		}
+		if f.Type().Name() == "bool" {
+			// This handles primarily the xxx_xx style fields in the return data from redis
+			if info[tag] != "" {
+				println(tag, ":=", info[tag])
+				val, err := strconv.ParseInt(info[tag], 10, 64)
+				if err != nil {
+					println("Unable to convert to data from sentinel server:", info[tag])
+					fmt.Println("Error:", err)
+				} else {
+					if val > 0 {
+						f.SetBool(true)
+					}
+				}
+			}
 		}
 	}
 	return
